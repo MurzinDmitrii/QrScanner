2fdbae1b1109a699e5f3de7d8196ad130b33c06a
MurzinDmitrii <114308567+MurzinDmitrii@users.noreply.github.com>
2024-10-25 16:13:54 +0300

    Работа со сканером

--- QrScanner/Program.cs
+++ QrScanner/Program.cs
@@ -1,57 +1,89 @@
 ﻿using System;
+using System.Text;
+using Gma.System.MouseKeyHook;
 using System.Diagnostics;
-using System.Drawing;
-using System.IO;
-using System.Timers;
-using AForge.Video;
-using AForge.Video.DirectShow;
-using ZXing;
-using ZXing.QrCode;
-using Timer = System.Timers.Timer;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Threading;
 
 class Program
 {
-    static FilterInfoCollection filterInfoCollection;
-    static VideoCaptureDevice captureDevice;
-    static Bitmap QrBitmap { get; set; }
+    static string result;
+    static IKeyboardMouseEvents globalHook;
+    static StringBuilder inputBuffer = new StringBuilder();
+    private static bool preventNewLine = false;
+
+    static Dictionary<char, char> translationMap = new Dictionary<char, char>()
+    {
+        {'й', 'q'}, {'ц', 'w'}, {'у', 'e'}, {'к', 'r'}, {'е', 't'},
+        {'н', 'y'}, {'г', 'u'}, {'ш', 'i'}, {'щ', 'o'}, {'з', 'p'},
+        {'х', '['}, {'ъ', ']'}, {'ф', 'a'}, {'ы', 's'}, {'в', 'd'},
+        {'а', 'f'}, {'п', 'g'}, {'р', 'h'}, {'о', 'j'}, {'л', 'k'},
+        {'д', 'l'}, {'ж', ';'}, {'э', '\''}, {'я', 'z'}, {'ч', 'x'},
+        {'с', 'c'}, {'м', 'v'}, {'и', 'b'}, {'т', 'n'}, {'ь', 'm'},
+        {'б', ','}, {'ю', '.'},
+        {'Й', 'Q'}, {'Ц', 'W'}, {'У', 'E'}, {'К', 'R'}, {'Е', 'T'},
+        {'Н', 'Y'}, {'Г', 'U'}, {'Ш', 'I'}, {'Щ', 'O'}, {'З', 'P'},
+        {'Х', '{'}, {'Ъ', '}'}, {'Ф', 'A'}, {'Ы', 'S'}, {'В', 'D'},
+        {'А', 'F'}, {'П', 'G'}, {'Р', 'H'}, {'О', 'J'}, {'Л', 'K'},
+        {'Д', 'L'}, {'Ж', ':'}, {'Э', '"'}, {'Я', 'Z'}, {'Ч', 'X'},
+        {'С', 'C'}, {'М', 'V'}, {'И', 'B'}, {'Т', 'N'}, {'Ь', 'M'},
+        {'Б', '<'}, {'Ю', '>'}, {'.', '/'}
+    };
 
     static void Main(string[] args)
     {
-        filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+        result = "";
+        System.Threading.Timer _timer = new System.Threading.Timer(ClearConsole, null, 0, 50);
+        Console.WriteLine("Программа запущена. Нажмите Ctrl + Q, чтобы завершить.");
 
-        VideoCaptureDevice captureDevice = new VideoCaptureDevice(filterInfoCollection[0].MonikerString);
-        captureDevice.NewFrame += CaptireDevice_NewFrame;
-        captureDevice.Start();
-        Timer timer = new Timer(500);
-        timer.Elapsed += TimedEvent;
-        timer.Enabled = true;
-        timer.Start();
+        globalHook = Hook.GlobalEvents();
+        globalHook.KeyDown += GlobalHookKeyDown;
+        globalHook.KeyPress += GlobalHookKeyPress;
 
+        // Ожидание завершения
+        Application.Run();
+
+        // Остановка перехватчика
+        globalHook.KeyDown -= GlobalHookKeyDown;
+        globalHook.KeyPress -= GlobalHookKeyPress;
+        globalHook.Dispose();
+
+        Console.WriteLine("\nПрограмма завершена.");
     }
 
-    private static void CaptireDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+    private static void GlobalHookKeyDown(object sender, KeyEventArgs e)
     {
-        QrBitmap = (Bitmap)eventArgs.Frame.Clone();
-    }
-
-    private static void TimedEvent(object source, ElapsedEventArgs e)
-    {
-        try
+        if (e.Control && e.KeyCode == Keys.Q)
         {
-            if (QrBitmap != null)
+            Application.Exit();
+        }
+    }
+
+    private static void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+    {
+        if (e.KeyChar == (char)13) // Enter
+        {
+            result = inputBuffer.ToString();
+            result = ConvertText(result);
+            
+            result = ExtractUrl(result);
+            Console.WriteLine("\nВведенная строка: " + result);
+            try
             {
-                BarcodeReader barcodeReader = new BarcodeReader();
-                Result result = barcodeReader.Decode(QrBitmap);
-                if (result != null)
-                {
-                    OpenUrlInBrowser(result.ToString());
-                    QrBitmap = null;
-                }
+                OpenUrlInBrowser(result);
+                inputBuffer.Clear();
+            }
+            catch
+            {
+                Console.WriteLine("Некорректный адрес");
             }
         }
-        catch (Exception ex)
+        else
         {
-            Console.WriteLine("Ошибка при чтении QR: " + ex.Message);
+            inputBuffer.Append(e.KeyChar);
+            Console.Write(e.KeyChar);
         }
     }
 
@@ -69,5 +101,39 @@ class Program
         {
             Console.WriteLine("Ошибка при открытии URL: " + ex.Message);
         }
+    }
+
+    static string ConvertText(string input)
+    {
+        char[] convertedChars = new char[input.Length];
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (translationMap.ContainsKey(input[i]))
+            {
+                convertedChars[i] = translationMap[input[i]];
+            }
+            else
+            {
+                convertedChars[i] = input[i];
+            }
+        }
+        return new string(convertedChars);
+    }
+
+    static string ExtractUrl(string input)
+    {
+        string pattern = @"(http[s]?://[\wа-яА-Я.:/?&=#-]+)";
+        Regex regex = new Regex(pattern);
+        Match match = regex.Match(input);
+        if (match.Success)
+        {
+            return match.Value;
+        }
+        return string.Empty;
+    }
+
+    static void ClearConsole(object state)
+    {
+        Console.Clear();
     }
 }
